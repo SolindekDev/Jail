cf1f0d6de0c907742dce605ebadebaefaed5a002
SolindekDev <83218819+SolindekDev@users.noreply.github.com>
2022-08-20 16:43:04 +0200

    Delete debug directory

--- debug/deps/jail_args_parser-afbf9dabbb837b54.d
+++
@@ -1,7 +0,0 @@
-/home/solindek/Documents/jail/compiler/jail/../../debug/deps/jail_args_parser-afbf9dabbb837b54.rmeta: /home/solindek/Documents/jail/compiler/jail_args_parser/src/lib.rs
-
-/home/solindek/Documents/jail/compiler/jail/../../debug/deps/libjail_args_parser-afbf9dabbb837b54.rlib: /home/solindek/Documents/jail/compiler/jail_args_parser/src/lib.rs
-
-/home/solindek/Documents/jail/compiler/jail/../../debug/deps/jail_args_parser-afbf9dabbb837b54.d: /home/solindek/Documents/jail/compiler/jail_args_parser/src/lib.rs
-
-/home/solindek/Documents/jail/compiler/jail_args_parser/src/lib.rs:
--- debug/deps/jail_ast-83e2251672da19e4.d
+++
@@ -1,7 +0,0 @@
-/home/solindek/Documents/jail/compiler/jail/../../debug/deps/jail_ast-83e2251672da19e4.rmeta: /home/solindek/Documents/jail/compiler/jail_ast/src/lib.rs
-
-/home/solindek/Documents/jail/compiler/jail/../../debug/deps/libjail_ast-83e2251672da19e4.rlib: /home/solindek/Documents/jail/compiler/jail_ast/src/lib.rs
-
-/home/solindek/Documents/jail/compiler/jail/../../debug/deps/jail_ast-83e2251672da19e4.d: /home/solindek/Documents/jail/compiler/jail_ast/src/lib.rs
-
-/home/solindek/Documents/jail/compiler/jail_ast/src/lib.rs:
